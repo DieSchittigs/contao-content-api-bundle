c68405d2c1e1defa42217018cede2224629b204b
Tobias Duehr <tobias@dieschittigs.de>
2017-05-05 10:19:15 +0200

    better image size handling and other improvements

---
+++ src/ModuleHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DieSchittigs\ContaoContentApi;
+
+use Contao\ModuleModel;
+use Contao\Module;
+
+class ModuleHelper
+{
+    public static function get($id)
+    {
+        $moduleModel = ModuleModel::findByPk($id);
+        $moduleClass = Module::findClass($moduleModel->type);
+        $module = new $moduleClass($moduleModel, $column);
+
+        $_module = Helper::toObj($moduleModel);
+        $_module->compiledHTML = Helper::replaceHTML(@$module->generate());
+
+        return $_module;
+    }
+}
